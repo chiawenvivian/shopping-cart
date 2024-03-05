a26d476e014ce182274a1671e49de8d8db86f5df
chiawenvivian <chiawenvivian@gmail.com>
2024-03-05 11:57:54 +0800

    update checkinfo page

--- src/views/CheckinfoView.vue
+++ src/views/CheckinfoView.vue
@@ -1,8 +1,111 @@
-<script >
+<script>
+import ProductCard from '@/components/ProductCard.vue';
+import Nav from '@/components/Nav.vue';
+import PublicBtn from '@/components/PublicBtn.vue';
+import PublicLabel2 from '@/components/PublicLabel2.vue';
+export default {
+    components:{
+        ProductCard,
+        Nav,
+        PublicBtn,
+        PublicLabel2,
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+    },
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <header>
+        <Nav/>
+    </header>
+    <main class="w-full pt-[50px] font-big">
+        <div class="w-full px-5">
+    <div>
+        <h1 class="mb-3 text-3xl font-bold text-white">完成購買</h1>
+    </div>
+    <div class="mb-6 px-5">
+      <div class="grid-tr grid-cols-6 grid py-1 border-b border-main-deep min-h-[80px] gap-x-3 md:gap-x-1 lg:first:border-t lg:!py-3">
+            <div class="flex justify-center">
+                
+            </div>
+            <div class="flex justify-center items-center text-center text-white font-bold">
+              商品圖片
+            </div>
+            <div class="flex items-center text-center text-white font-bold">
+                商品名稱
+            </div>
+            <div class="gird-td flex justify-center items-center text-center text-white font-bold"> 價錢</div>
+            <div class="gird-td flex justify-center items-center lg:flex-wrap lg:!justify-between lg:pt-6 md:!pt-0">
+                <div class="flex justify-center items-center text-center text-white font-bold">數量</div>
+            </div>
+        </div>
+        <div class="grid-tr grid-cols-6 grid py-1 border-b border-main-deep min-h-[80px] gap-x-3 md:gap-x-1 lg:first:border-t lg:!py-3">
+            <div class="flex justify-center">
+                
+            </div>
+            <div class="flex justify-center">
+                <img src="https://dummyimage.com/100x100" alt="商品圖示" class="w-[100px] h-full object-cover">
+            </div>
+            <div class="flex items-center">
+                Alphazap
+            </div>
+            <div class="gird-td flex justify-center items-center"> $776</div>
+            <div class="gird-td flex justify-center items-center lg:flex-wrap lg:!justify-between lg:pt-6 md:!pt-0">
+                <label>
+                    <input type="number" value="1" class="text-black
+                    text-center">
+                </label>
+            </div>
+        </div>
+        <div class="grid-tr grid-cols-6 grid py-1 border-b border-main-deep min-h-[80px] gap-x-3 md:gap-x-1 lg:first:border-t lg:!py-3">
+            <div class="flex justify-center">
+                
+            </div>
+            <div class="flex justify-center">
+                <img src="https://dummyimage.com/100x100" alt="商品圖示" class="w-[100px] h-full object-cover">
+            </div>
+            <div class="flex items-center">
+                Bytecard
+            </div>
+            <div class="gird-td flex justify-center items-center"> $954</div>
+            <div class="gird-td flex justify-center items-center lg:flex-wrap lg:!justify-between lg:pt-6 md:!pt-0">
+                <label>
+                    <input type="number" value="1" class="text-black
+                    text-center">
+                </label>
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="mb-6 px-5">
+    <div class="grid-cols-6 grid border-y border-main-deep text-main-deep font-bold py-1 gap-x-3">
+      <div class="gird-th">姓名</div>
+      <div class="gird-th">電話</div>
+      <div class="gird-th">地址</div>
+      <div class="gird-th">付款方式</div>
+      <div class="gird-th">信箱</div>
+      <div class="gird-th">運送方式</div>
+    </div>
+    <div class="grid-tr grid-cols-6 grid py-1 border-b border-main-deep min-h-[80px] gap-x-3 md:gap-x-1 lg:first:border-t lg:!py-3">
+      <div class="flex items-center"></div>
+      <div class="flex items-center"></div>
+      <div class="flex items-center"></div>
+      <div class="flex items-center"></div>
+      <div class="flex items-center"></div>
+      <div class="flex items-center"></div>
+    </div>
+  </div>
+  <a href="#/shopping" class="flex justify-center items-center bg-[#50468c] text-white rounded-md px-4 py-1 cursor-pointer md:p-0"><span> 回首頁 </span></a>
+    </main>
 </template>
+
+<style scoped></style>
