438dcda22899806a39ded3ef5c4af3e02e35363f
chiawenvivian <chiawenvivian@gmail.com>
2024-03-11 09:14:31 +0800

    debug

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -24,7 +24,7 @@ export default {
     },
 
     methods: {
-        ...mapActions(useShoppingCart,['countTotal','restCartData']),
+        ...mapActions(useShoppingCart,['countTotal','resetCartData']),
 
         submitCart(){
             this.restCartData();
